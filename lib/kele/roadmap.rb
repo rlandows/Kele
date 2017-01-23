8d1868f3ac21cff7e394538fe250ec42cf9c3143
rlandows <rlandows@uci.edu>
2017-01-22 22:13:02 -0800

    checkpoint-5

--- lib/kele/roadmap.rb
+++ lib/kele/roadmap.rb
@@ -1,17 +1,18 @@
-require 'httparty'
-require 'json'
-require 'kele'
-
 module Roadmap
   def get_roadmap(roadmap_id)
-    url = api_url('roadmaps/#{roadmap_id.to_s}')
+    url = api_url('roadmaps/#{roadmap_id}')
     response = self.class.get(url, headers: {"authorization" => @auth_token})
     body = JSON.parse(response.body)
   end
 
   def get_checkpoint(checkpoint_id)
-    url = api_url('checkpoints/#{checkpoint_id.to_s}')
+    url = api_url('checkpoints/#{checkpoint_id}')
     response = self.class.get(url, headers: {"authorization" => @auth_token})
     body = JSON.parse(response.body)
   end
+
+  private
+   def api_url(endpoint)
+     "https://www.bloc.io/api/v1/#{endpoint}"
+   end
 end
