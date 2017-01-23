169c872fa94754de93e8d7dc6a86dfa13c1e1bd0
rlandows <rlandows@uci.edu>
2017-01-22 22:30:29 -0800

    checkpoint-5

--- lib/kele/roadmap.rb
+++ lib/kele/roadmap.rb
@@ -1,12 +1,12 @@
 module Roadmap
   def get_roadmap(roadmap_id)
-    url = 'https://www.bloc.io/api/v1/roadmaps/' + (roadmap_id.to_s)
+    url = api_url("roadmaps/#{roadmap_id}")
     response = self.class.get(url, headers: {"authorization" => @auth_token})
     body = JSON.parse(response.body)
   end
 
   def get_checkpoint(checkpoint_id)
-    url = 'https://www.bloc.io/api/v1/checkpoints/' + (checkpoint_id.to_s)
+    url = api_url("checkpoints/#{checkpoint_id}")
     response = self.class.get(url, headers: {"authorization" => @auth_token})
     body = JSON.parse(response.body)
   end
